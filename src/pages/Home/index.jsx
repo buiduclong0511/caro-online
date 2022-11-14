937dc06c5fb7a65f5de8891f73d7bf373b7ebb15
DucLong <duclong@fullstack.edu.vn>
2022-11-14 13:19:14 +0700

    Remve unused code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,10 @@
-import { useEffect } from 'react';
 import { signOut } from '~/firebase/authentication';
 import { cx } from '~/util';
-import db from '~/firebase/database';
-import { doc, getDoc, getFirestore } from 'firebase/firestore';
 
 function Home() {
-    useEffect(() => {
-        db.store('products', {
-            name: 'product 2',
-            userRef: doc(getFirestore(), 'users/jMB4cCLGO62EtmVWFFRY'),
-        });
-    }, []);
-
     return (
         <h1 className={cx('text-center')}>
             <button onClick={signOut}>Logout</button>
-            <button
-                onClick={() => {
-                    db.index('products').then((res) =>
-                        res.forEach(async (item) => {
-                            console.log(await (await getDoc(item.userRef)).data());
-                        }),
-                    );
-                }}
-            >
-                Get
-            </button>
         </h1>
     );
 }
