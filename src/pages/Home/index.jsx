20a7c1f5212b63702f8e5d24dc89500afc62b30d
Duy Nguyen <duynguyen@fullstack.edu.vn>
2022-11-18 16:55:19 +0700

    Handle clear user when logout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,19 @@
+import { useDispatch } from 'react-redux';
 import { signOut } from '~/firebase/authentication';
+import { clearUser } from '~/redux/slices';
 import { cx } from '~/util';
 
 function Home() {
+    const dispatch = useDispatch();
+
+    const handleSignOut = () => {
+        dispatch(clearUser());
+        signOut();
+    };
+
     return (
         <h1 className={cx('text-center')}>
-            <button onClick={signOut}>Logout</button>
+            <button onClick={handleSignOut}>Logout</button>
         </h1>
     );
 }
